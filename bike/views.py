cb44c78748d6c202785a1e1d89fd6548a9923641
SvenDo11 <sven.donnerhak@live.com>
2023-09-26 02:21:29 +0200

    Got proper site layout. Added filter

--- bike/views.py
+++ bike/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django_filters.views import FilterView
 from .models import Bike, Fuel
 from .plots import get_test_plot
+import django_filters as dj_filters
 
 bike_fields = ["name", "manufacturer", "initial_odometer"]
 refueling_fields = ["bike", "fuel_date", "quantity", "cost_per_litre", "total_cost", "km_trip", "km_total"]
+
 
 class IndexView(generic.TemplateView):
     template_name = "bike/index.html"
@@ -17,17 +20,36 @@ class IndexView(generic.TemplateView):
         return context
 
 
-class RefuelingListView(generic.ListView):
+class RefuelingFilter(dj_filters.FilterSet):
+    fuel_date__lt = dj_filters.DateTimeFilter(field_name="fuel_date", lookup_expr='lt', label="From")
+    fuel_date__gt = dj_filters.DateTimeFilter(field_name="fuel_date", lookup_expr='gt', label="Till")
+
+    class Meta:
+        model = Fuel
+        fields = ['bike']
+
+
+class RefuelingListView(FilterView):
+    model = Fuel
     template_name = 'bike/refueling_list.html'
     context_object_name = 'fueling_list'
+    filterset_class = RefuelingFilter
+    showClear = False
+
+    def get(self, request, *args, **kwargs):
+        self.showClear = any(field in request.GET for field in
+                             set(self.filterset_class.get_fields()))
+        return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['navigationbar_active'] = "refueling"
-        return context
+        if self.showClear:
+            context['display_clear'] = "true"
+        for obj in context['object_list']:
+            print(obj.fuel_date)
 
-    def get_queryset(self):
-        return Fuel.objects.order_by('-fuel_date')
+        return context
 
 
 class RefuelingDetailView(generic.DetailView):
@@ -134,4 +156,3 @@ class BikeUpdateView(generic.UpdateView):
         context = super().get_context_data(**kwargs)
         context['navigationbar_active'] = "bikes"
         return context
-
